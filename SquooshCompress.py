60de06c73abcd96086fc6992980e75fe6e116da4
陈建祥 <853409335@qq.com>
2024-06-12 19:35:03 +0800

    增加移动到统一的目录下面

--- SquooshCompress.py
+++ SquooshCompress.py
@@ -3,6 +3,33 @@ import subprocess
 import shutil
 
 
+def move_files_to_total(source_dir, total_dir):
+    # 确保目标目录total存在
+    if not os.path.exists(total_dir):
+        os.makedirs(total_dir)
+
+    # 递归遍历源目录
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            source_file_path = os.path.join(root, file)
+            destination_file_path = os.path.join(total_dir, file)
+
+            # 如果目标文件已经存在，重新命名以避免冲突
+            if os.path.exists(destination_file_path):
+                base, extension = os.path.splitext(file)
+                i = 1
+                new_file_name = f"{base}_{i}{extension}"
+                destination_file_path = os.path.join(total_dir, new_file_name)
+                while os.path.exists(destination_file_path):
+                    i += 1
+                    new_file_name = f"{base}_{i}{extension}"
+                    destination_file_path = os.path.join(total_dir, new_file_name)
+
+            # 复制文件
+            shutil.copy2(source_file_path, destination_file_path)
+            print(f"移动文件: {source_file_path} 到 {destination_file_path}")
+
+
 def compress_images(input_dir, output_dir, quality=75):
     # 检查输入和输出目录是否存在
     if not os.path.exists(input_dir):
@@ -15,7 +42,7 @@ def compress_images(input_dir, output_dir, quality=75):
     for file in files:
         input_file = os.path.join(input_dir, file)
         temp_output_file = os.path.join(output_dir, "temp_" + file)
-        final_output_file = os.path.join(output_dir,file)
+        final_output_file = os.path.join(output_dir, file)
 
         # 检查是否为文件而非子目录
         if os.path.isfile(input_file):
@@ -45,11 +72,13 @@ def compress_images(input_dir, output_dir, quality=75):
 
             except subprocess.CalledProcessError as e:
                 print(f"Error compressing {input_file}: {e}")
+    move_files_to_total(output_dir,total_directory)
 
 
 if __name__ == "__main__":
     input_directory = "/Users/chenjianxiang/Downloads/input"
     output_directory = "/Users/chenjianxiang/Downloads/output"
+    total_directory = "/Users/chenjianxiang/Downloads/all_compressed_files"
     if os.path.exists(output_directory):
         shutil.rmtree(output_directory)
     if os.path.exists(input_directory):
